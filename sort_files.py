ee1a6d39c35973d73fbe9774603ac5a556b505c9
oquidave <oquidave@gmail.com>
2013-10-27 21:57:53 +0300

    Update sort_files.py

--- sort_files.py
+++ sort_files.py
@@ -1,14 +1,20 @@
+"""
+
+
+"""
+
+
 import os
 from fnmatch import fnmatch
 from shutil import move
 from time import sleep
 
-dir_path="/home/oquidave/hdrecovers"
-dest_path="/home/oquidave/images"
+dir_path="/source/directory"
+dest_path="/destination/directory"
 for dirpath, dirs, files in os.walk(dir_path):
   for file in files:
     print file
-    if fnmatch(file, "*.iso"):
+    if fnmatch(file, "*.your_file_extension"):
       file_fullpath = os.path.join(dirpath, file)
       move(file_fullpath, dest_path)
       print "Moved movie file: : %s " % file_fullpath
