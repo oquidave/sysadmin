1e5b8289c49e31c9c411c656775e68209d88b39d
oquidave <oquidave@gmail.com>
2014-04-03 16:03:00 +0300

    added copyright info

--- remote_shh_cmd.py
+++ remote_shh_cmd.py
@@ -19,6 +19,11 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
+@Author: David Okwii
+@Date: 2013
+@Email: oquidave@gmail.com
+@dependencies: Python 2.7 or higher, Paramiko module
+
 """
 
 
